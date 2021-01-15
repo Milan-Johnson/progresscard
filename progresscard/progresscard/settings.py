cd75c1b7412912f4f624dc50fd9df5dcefcaafd2
Milan Johnson <milanjohnsonijk@gmail.com>
2021-01-16 01:45:02 +0530

    db created for storing marks

--- progresscard/progresscard/settings.py
+++ progresscard/progresscard/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-   
-    'marks.apps.MarksConfig',
+    'accounts',
+    
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- progresscard/students/models.py
+++ progresscard/students/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class studentmarks(models.Model):
 
-    user = models.CharField(max_length=50)
+class marks(models.Model):
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     malayalam = models.SmallIntegerField()
-    englih = models.SmallIntegerField()
+    english = models.SmallIntegerField()
     hindi = models.SmallIntegerField()
     maths = models.SmallIntegerField()
     science = models.SmallIntegerField()
